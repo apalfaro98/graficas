97ea0ab749a276a40c834546f594579604faca4c
Angelica Perez <apalfaro9803@gmail.com>
2022-10-08 07:07:52 -0400

    Add the App Bar

---
+++ src/components/AppBar.vue
@@ -0,0 +1,21 @@
+<script>
+export default {};
+</script>
+
+<template>
+    <div
+        class="w-screen h-12 bg-blue-400 flex justify-between items-center text-lg text-white"
+    >
+        <div>
+            <button>Menu</button>
+            <div
+                class="inline-block ml-16 mr-1 h-4 w-4 bg-red-600 rounded-full"
+            ></div>
+            <button class="">User Name</button>
+        </div>
+        <div>
+            <button>Sales Report</button>
+        </div>
+        <div></div>
+    </div>
+</template>
